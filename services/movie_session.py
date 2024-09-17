a980ce947add47cd23ca69c117cf67c0ad8acaf9
Mykola <jacktyler2391@gmail.com>
2024-09-17 10:06:45 +0300

    Solution

---
+++ services/movie_session.py
@@ -0,0 +1,64 @@
+import time
+from datetime import datetime
+
+from django.db.models import QuerySet
+
+from db.models import MovieSession, Movie, CinemaHall
+
+
+def create_movie_session(
+        movie_show_time: datetime,
+        movie_id: int = None,
+        cinema_hall_id: int = None,
+) -> MovieSession:
+    movie = Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
+    return MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=movie,
+        cinema_hall=cinema_hall
+    )
+
+
+def get_movies_sessions(session_date: str = None) -> QuerySet:
+
+    if session_date:
+        return MovieSession.objects.filter(
+            show_time__date=session_date
+        )
+
+    return MovieSession.objects.all()
+
+
+def get_movie_session_by_id(session_id: int,
+                            show_time: str = None,
+                            movie_id: int = None,
+                            cinema_hall_id: int = None) -> MovieSession:
+    return MovieSession.objects.get(id=session_id)
+
+
+def update_movie_session(
+        session_id: int,
+        show_time: str = None,
+        movie_id: int = None,
+        cinema_hall_id: int = None
+) -> None | MovieSession:
+
+    session = get_movie_session_by_id(session_id)
+
+    if show_time:
+        session.show_time = show_time
+
+    if movie_id:
+        session.movie = Movie.objects.get(id=movie_id)
+
+    if cinema_hall_id:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    session.save()
+
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> None:
+    movie_session = MovieSession.objects.filter(id=session_id).delete()
